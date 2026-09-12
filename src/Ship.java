f19e46aa0dbd592135556add4d8617faec445fc2
Dylan <dylan.hason.ho@gmail.com>
2026-09-11 18:19:38 -0700

    added ship class

--- src/Ship.java
+++ src/Ship.java
@@ -1,3 +1,32 @@
 public class Ship {
+    private String shipName;
+    private String shipCreationDate;
+
+    public Ship(String shipName,String shipCreationDate){
+        this.shipName = shipName;
+        this.shipCreationDate = shipCreationDate;
+    }
+
+    public void setShipName(String shipName) {
+        this.shipName = shipName;
+    }
+
+    public void setShipCreationDate(String shipCreationDate) {
+        this.shipCreationDate = shipCreationDate;
+    }
+
+    public String getShipName() {
+        return shipName;
+    }
+
+    public String getShipCreationDate() {
+        return shipCreationDate;
+    }
+
+    public void print()
+    {
+        System.out.println("Ship: " + shipName);
+        System.out.println("Ship creation date:" + shipCreationDate);
+    }
 
 }
