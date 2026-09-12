936a4cab87eeb274264fa7f43ba2c5d7cc37bdcb
Dylan <dylan.hason.ho@gmail.com>
2026-09-11 19:22:08 -0700

    changed CourseDriver and Course classes to demonstrate that textbook and instructor can be independent of a course object, and that course has a textbook and instructor.

--- src/CourseDriver.java
+++ src/CourseDriver.java
@@ -4,10 +4,16 @@ public class CourseDriver {
         Instructor instructor2 = new Instructor("instructor", "Two", "10-1234");
 
         Textbook textbook1 = new Textbook("Clean Code", "Robert C. Martin", "Prentice Hall");
-        Textbook textbook2 = new Textbook("Book2", "Author", "PublisherTwo");
+        Textbook textbook2 = new Textbook("Book2", "AuthorTwo", "PublisherTwo");
 
-        Course course = new Course("CS 3560", instructor1, instructor2, textbook1, textbook2);
+        Course course = new Course("CS 3560", instructor1, textbook1);
 
         course.print();
+        System.out.println();
+
+        System.out.println("Instructor Two Name and Office: ");
+        System.out.println("Name: " + instructor2.getFirstName() + " " + instructor2.getLastName() + " Office: " + instructor2.getOfficeNumber());
+        System.out.println("Textbook two title, author, and publisher:");
+        System.out.println("Title: " + textbook2.getTitle() + " Author: " + textbook2.getAuthor() + " Publisher: " + textbook2.getPublisher());
     }
 }
